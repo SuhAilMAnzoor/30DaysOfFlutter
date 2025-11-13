fd3d83f859ee1294d7f32a00864b99446cd04505
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2025-11-13 21:37:24 +0500

    Converted in Provider State Mangement and remove  GridView.builder for Web Support and had some bugs which has been resolved

--- lib/pages/home/home_widgets/catalog_list_and_item.dart
+++ lib/pages/home/home_widgets/catalog_list_and_item.dart
@@ -6,48 +6,26 @@ import 'package:flutter_catalog/pages/home/home_widgets/add_to_cart.dart';
 import 'package:flutter_catalog/pages/home/home_widgets/catalog_image.dart';
 
 class CatalogList extends StatelessWidget {
-  const CatalogList({super.key, required items});
+  const CatalogList({super.key, required List<Item> items});
 
   @override
   Widget build(BuildContext context) {
-    return kIsWeb
-        ? GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 20,
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: CatalogModel.items.length,
+      itemBuilder: (context, index) {
+        final catalog = CatalogModel.items[index];
+        return InkWell(
+          onTap: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => HomeDetailPage(catalog: catalog),
             ),
-
-            shrinkWrap: true,
-            itemCount: CatalogModel.items.length,
-            itemBuilder: (context, index) {
-              final catalog = CatalogModel.items[index];
-              return InkWell(
-                onTap: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => HomeDetailPage(catalog: catalog),
-                  ),
-                ),
-                child: CatalogItem(catalog: catalog),
-              );
-            },
-          )
-        : ListView.builder(
-            shrinkWrap: true,
-            itemCount: CatalogModel.items.length,
-            itemBuilder: (context, index) {
-              final catalog = CatalogModel.items[index];
-              return InkWell(
-                onTap: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => HomeDetailPage(catalog: catalog),
-                  ),
-                ),
-                child: CatalogItem(catalog: catalog),
-              );
-            },
-          );
+          ),
+          child: CatalogItem(catalog: catalog),
+        );
+      },
+    );
   }
 }
 
@@ -58,62 +36,6 @@ class CatalogItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var children = [
-      Hero(
-        tag: Key(catalog.id.toString()),
-        child: CatalogImage(image: catalog.image),
-      ),
-      Expanded(
-        child: Padding(
-          padding: EdgeInsets.all(
-            MediaQuery.of(context).size.width < 600 ? 0 : 16,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                catalog.name,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 14,
-                  color: Theme.of(context).colorScheme.secondary,
-                ),
-              ),
-              Text(
-                catalog.desc,
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.secondary,
-                ),
-              ),
-              ButtonBar(
-                alignment: MainAxisAlignment.spaceBetween,
-                buttonPadding: EdgeInsets.zero,
-                children: [
-                  Text(
-                    "\$${catalog.price.toString()}",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                      color: Theme.of(context).colorScheme.secondary,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 130,
-                    height: 40,
-                    child: Padding(
-                      padding: const EdgeInsets.only(right: 4.0),
-                      // we make it a stateful widget so that's why we extract a new widget for this button
-                      child: AddToCart(catalog: catalog),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    ];
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 14.0),
       child: Container(
@@ -130,9 +52,59 @@ class CatalogItem extends StatelessWidget {
             ),
           ],
         ),
-        child: MediaQuery.of(context).size.width < 600
-            ? Row(children: children)
-            : Column(children: children),
+        child: Row(
+          children: [
+            Hero(
+              tag: Key(catalog.id.toString()),
+              child: CatalogImage(image: catalog.image),
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    catalog.name,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                  ),
+                  Text(
+                    catalog.desc,
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                  ),
+                  ButtonBar(
+                    alignment: MainAxisAlignment.spaceBetween,
+                    buttonPadding: EdgeInsets.zero,
+                    children: [
+                      Text(
+                        "\$${catalog.price.toString()}",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: Theme.of(context).colorScheme.secondary,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 130,
+                        height: 40,
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 4.0),
+                          // we make it a stateful widget so that's why we extract a new widget for this button
+                          child: AddToCart(catalog: catalog),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
